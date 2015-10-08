00876cd7804619638cf590706b070f5633acd896
den <den@informunity.ru>
2015-10-08 10:32:31 +0300

    Добавлен новый ассерт

--- exc3.php
+++ exc3.php
@@ -31,5 +31,6 @@ assert(numEndings(0,array('яблоко','яблока', 'яблок')) == 'яб
 assert(numEndings(3,array('яблоко','яблока', 'яблок')) == 'яблока');
 assert(numEndings(1,array('яблоко','яблока', 'яблок')) == 'яблоко');
 assert(numEndings(18,array('яблоко','яблока', 'яблок')) == 'яблок');
+assert(numEndings(12,array('яблоко','яблока', 'яблок')) == 'яблок');
 
 ?>
