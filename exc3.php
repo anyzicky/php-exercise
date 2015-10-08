8a1909604a3d4a8372b746489a297b7ea48d05a8
den <den@informunity.ru>
2015-10-08 10:50:47 +0300

    Исправлено от 10 до 20

--- exc3.php
+++ exc3.php
@@ -9,20 +9,27 @@
 
 function numEndings($num, array $endings) {
 
-	switch ($num) {
-		case '1':
-			return $endings[0];
-			break;
-		case '2':
-		case '3':
-		case '4':
-			return $endings[1];
-			break;
+	$num = $num % 100;
+	if($num >= 11 && $num <= 19){
+		return $endings[2];
+	} else {
+		$num = $num % 10;
+		switch ($num) {
+			case '1':
+				return $endings[0];
+				break;
+			case '2':
+			case '3':
+			case '4':
+				return $endings[1];
+				break;
 
-		default:
-			return $endings[2];
-			break;
+			default:
+				return $endings[2];
+				break;
+		}
 	}
+
 
 }
 
@@ -32,5 +39,7 @@ assert(numEndings(3,array('яблоко','яблока', 'яблок')) == 'яб
 assert(numEndings(1,array('яблоко','яблока', 'яблок')) == 'яблоко');
 assert(numEndings(18,array('яблоко','яблока', 'яблок')) == 'яблок');
 assert(numEndings(12,array('яблоко','яблока', 'яблок')) == 'яблок');
+assert(numEndings(101,array('яблоко','яблока', 'яблок')) == 'яблоко');
+assert(numEndings(32,array('яблоко','яблока', 'яблок')) == 'яблока');
 
 ?>
